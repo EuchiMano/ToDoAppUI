0792b791ef394dc99ac5c1874ff4856203cd4745
EuchiMano <marce.ps666@gmail.com>
2023-12-08 14:52:52 -0400

    add crud components

---
+++ src/app/models/todo.model.ts
@@ -0,0 +1,9 @@
+export interface Todo{
+    id: string,
+    description: string,
+    isCompleted: boolean,
+    creationDate: Date,
+    completedDate: Date,
+    isDeleted: boolean,
+    deletedDate: Date
+}
